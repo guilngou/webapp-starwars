5049aa74c90fab16542c907e1062af6e50c463f4
Alexandre <alexandreguilngar@gmail.com>
2018-10-24 18:23:37 +0200

    added element UI

--- src/views/ResourceList.vue
+++ src/views/ResourceList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Resources Listing</h1>
+    <el-radio-group v-model="filter" size="medium">
+      <el-radio-button label="Characters"></el-radio-button>
+      <el-radio-button label="Books"></el-radio-button>
+      <el-radio-button label="Houses"></el-radio-button>
+    </el-radio-group>
     <ResourceCard />
   </div>
 </template>
@@ -15,15 +20,15 @@ export default {
   },
   data() {
     return {
-      resources: []
+      filter: 'Characters'
     }
   },
   created() {
     axios
-      .get('https://www.anapioficeandfire.com/api')
+      .get('https://www.anapioficeandfire.com/api/' + this.filter)
       .then(response => {
-        this.resources = response.data
-        console.log(response)
+        //this.resourceTypes = Object.keys(response.data)
+        console.log(response.data)
       })
       .catch(error => {
         console.error(error)
